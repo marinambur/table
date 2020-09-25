47325ddc4507fbf766fe41313f2f9bd8bb5c0a9a
marinambur <marinaburykina@gmail.com>
2020-09-25 21:50:37 +0300

    request is added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+
+
 import React, { Component } from 'react';
 
 
 class App extends Component {
 
-  render() {
-    return (
-        <div className="container">
-          MyApp
-        </div>
-    );
-  }
+    async componentDidMount() {
+        const response = await fetch(`http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
+        const data = await response.json()
+        console.log(data)
+    }
+    render() {
+        return (
+            <div className="container">
+                MyApp
+            </div>
+        );
+    }
 }
 
 export default App;
